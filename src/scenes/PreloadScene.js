e339e0c61f2967df4d7c279d76670c64a66a280d
enriquetejeda <quique.tejeda.68@gmail.com>
2024-12-26 23:09:13 -0500

    fix: delete public/ from path of assets

--- src/scenes/PreloadScene.js
+++ src/scenes/PreloadScene.js
@@ -1,19 +1,19 @@
 export default class PreloadScene extends Phaser.Scene {
-    
+
   constructor() {
     super('PreloadScene');
   }
 
   preload() {
-    this.load.image('title', 'public/assets/images/title.png');
-    this.load.image('player', 'public/assets/images/player.png');
-    this.load.image('obstacle', 'public/assets/images/obstacle.png');
-    this.load.image('cheese', 'public/assets/images/cheese.png');
-    this.load.image('background', 'public/assets/images/background.jpg');
-    this.load.image('ground', 'public/assets/images/ground.png');
-    this.load.image('leftButton', 'public/assets/images/button-left.png');
-    this.load.image('rightButton', 'public/assets/images/button-right.png');
-    this.load.image('jumpButton', 'public/assets/images/button-jump.png');
+    this.load.image('title', 'assets/images/title.png');
+    this.load.image('player', 'assets/images/player.png');
+    this.load.image('obstacle', 'assets/images/obstacle.png');
+    this.load.image('cheese', 'assets/images/cheese.png');
+    this.load.image('background', 'assets/images/background.jpg');
+    this.load.image('ground', 'assets/images/ground.png');
+    this.load.image('leftButton', 'assets/images/button-left.png');
+    this.load.image('rightButton', 'assets/images/button-right.png');
+    this.load.image('jumpButton', 'assets/images/button-jump.png');
   }
 
   create() {
