2c67a0743bda81d86e82c00c1c96ee6d4e6ed460
enriquetejeda <quique.tejeda.68@gmail.com>
2024-12-28 21:46:14 -0500

    fix: margin padding reset

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ const config = {
   width: window.innerWidth,
   height: window.innerHeight,
   parent: '',
+  pixelArt: true,
   scale: {
     mode: Phaser.Scale.FIT,
     autoCenter: Phaser.Scale.CENTER_BOTH,
